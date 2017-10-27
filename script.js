9f1c3b97dfd6c64c25c3c4d2e8cfa81fdebc4c62
andrewreid89 <32715271+andrewreid89@users.noreply.github.com>
2017-10-27 18:49:25 -0400

    final commit for Part 2

--- script.js
+++ script.js
@@ -1,49 +1,58 @@
-var User = null;
-var UserWins = 0;
-var GrantPoints = 10;
-var UserPoints = 40;
+StartGame();
 
-
-var Playing = prompt("Do you want to fight a chicken? Type 'Yes' or 'No'");
-
-   
-if(Playing==="No"){
-    alert("You are a chicken that you wouldn't fight a chicken...");
-}
-
-if(Playing === "Yes"){
-    var User = prompt("What do you want your username to be?");
-}
-
-
-
-for(var i=1;i<=10;i++){
+function StartGame(){
     
-GrantPoints=10;
+    
+    var YN = prompt("Do you want to fight a chicken?  Type 'Yes' or 'No'");
+    if(YN === "yes"||YN === "Yes") {
+        var User = prompt("What do you want your username to be?");
+        console.log(User);
+    } else if (YN === "no"||YN === "No") {
+        alert("You are a chicken that you wouldn't fight a chicken...");
+    }   
+} 
 
-while(GrantPoints>0 && UserPoints>0){
-    Hit = Math.floor((Math.random() * 2) + 1);
-    GrantPoints = (GrantPoints - Hit);
-    Hit = Math.floor((Math.random() * 2) + 1);
-    UserPoints = (UserPoints - Hit);
-    console.log("Grant has " + GrantPoints + " health left.");
-    console.log(User + " has " + UserPoints + " health left.");   
+//I tried to have User added to the strings below ("You defeated Grant" --> User + " defeated Grant") but it kept coming back saying User was undefined.  I have no idea why and could really use help.
+
+
+var UserHealth = 40;
+var GrantHealth = 10;
+var UserWins = 0;
+
+
+
+function getDamage(){
+    return Math.floor(Math.random() * 5) + 1;
 }
- 
-    if(GrantPoints<=0){
-        UserWins++;}
+
+
+while(UserHealth>0 && UserWins<3){
+    var AoQ = prompt("Would you like to attack or quit?");
+    
+    if(AoQ === "attack"||AoQ === "Attack"){
+        StartCombat();
+    } else if(AoQ === "quit"||AoQ === "Quit"){
+        alert(User + " just ran from a chicken");
+    }
+}
+
+function StartCombat(){
+    UserHealth -= getDamage();
+    console.log("'s remaining health is " + UserHealth);
+    GrantHealth -= getDamage();
+    console.log("Grant's remaining health is " + GrantHealth);
+    
+    if(GrantHealth<=0){
+        UserWins++;
+        GrantHealth = 10;
+        console.log("You defeated Grant! You have " + UserWins + " victory(s) and " + UserHealth + " health remaining.");
+    }
 
     if(UserWins===3){
-        alert(User + " KILLED THE CHICKEN!!!");
-        break;
+        alert("YOU KILLED THE CHICKEN!!!");
     }
-    
-    if(UserPoints<=0){
-        alert(User + ".... lost to a chicken...  " + User + " sucks");
-        break;
-    }
-    
-   if(UserWins<3){
-       alert(User + " defeated Grant!  " + User + "'s remaining health is " + UserPoints + ".  " + User + " has " + UserWins + " win(s).");
+
+    if(UserHealth<=0){
+        alert("you.... lost to a chicken...");
     }
 }
