d42177d27e0872804b13e4555ff1697c336b6b6f
andrewreid89 <32715271+andrewreid89@users.noreply.github.com>
2017-10-31 19:16:24 -0400

    Final Commit for Part 3

--- script.js
+++ script.js
@@ -1,58 +1,92 @@
-StartGame();
+//Start Game - Getting the User's Name and desire to fight//
 
-function StartGame(){
-    
-    
-    var YN = prompt("Do you want to fight a chicken?  Type 'Yes' or 'No'");
-    if(YN === "yes"||YN === "Yes") {
-        var User = prompt("What do you want your username to be?");
-        console.log(User);
-    } else if (YN === "no"||YN === "No") {
-        alert("You are a chicken that you wouldn't fight a chicken...");
-    }   
-} 
+var YN = prompt("Do you want to fight a chicken?  Type 'Yes' or 'No'");
 
-//I tried to have User added to the strings below ("You defeated Grant" --> User + " defeated Grant") but it kept coming back saying User was undefined.  I have no idea why and could really use help.
-
-
-var UserHealth = 40;
-var GrantHealth = 10;
-var UserWins = 0;
+if(YN === "yes"||YN === "Yes") {
+    var UserName = prompt("What do you want your character's name to be?");
+    }
+else if (YN === "no"||YN === "No") {
+    alert("You are a chicken that you wouldn't fight a chicken...");
+    }
 
 
 
-function getDamage(){
-    return Math.floor(Math.random() * 5) + 1;
-}
+//Setting up objects for User and Grant//
 
-
-while(UserHealth>0 && UserWins<3){
-    var AoQ = prompt("Would you like to attack or quit?");
-    
-    if(AoQ === "attack"||AoQ === "Attack"){
-        StartCombat();
-    } else if(AoQ === "quit"||AoQ === "Quit"){
-        alert(User + " just ran from a chicken");
+var User = {
+    name: UserName,
+    UserHealth: 40,
+    HealsRemaining: 2,
+    UserWins: 0,  
+    generateAttackDamage: function(){
+        return Math.floor(Math.random() * 3) + 1;
+    },
+    Heal: function(){
+        return Math.floor(Math.random() * 10) + 1;
     }
 }
 
-function StartCombat(){
-    UserHealth -= getDamage();
-    console.log("'s remaining health is " + UserHealth);
-    GrantHealth -= getDamage();
-    console.log("Grant's remaining health is " + GrantHealth);
+
+var Grant = {
+    name: "Grant",
+    GrantHealth: 10,
+    generateAttackDamage: function(){
+        return Math.floor(Math.random() * 3) + 1;
+    }
+}
+
+
+Decision();
+
+function Decision(){
     
-    if(GrantHealth<=0){
-        UserWins++;
-        GrantHealth = 10;
-        console.log("You defeated Grant! You have " + UserWins + " victory(s) and " + UserHealth + " health remaining.");
+    if(User.UserHealth>0 && User.UserWins<5){
+        
+        
+        var gameChoice = prompt("Do you want to attack, heal, or quit?");
+        
+        
+        if(gameChoice === "attack"){
+            Grant.GrantHealth -= Grant.generateAttackDamage();
+            console.log(User.name + " attacked Grant and reduced his health to " + Grant.GrantHealth);
+            User.UserHealth -= User.generateAttackDamage();
+            console.log("Grant attacked " + User.name + " and reduced their health to " + User.UserHealth);
+            if(Grant.GrantHealth<=0){
+                User.UserWins++;
+                Grant.GrantHealth = 10;
+            };
+            console.log("Grant's health is " + Grant.GrantHealth);
+            console.log("User Wins is " + User.UserWins);
+            Decision();
+        };
+        
+        
+        if(gameChoice === "heal" && User.HealsRemaining !==0){
+            User.UserHealth += User.Heal();
+            console.log(User.name+ "'s health increased to " + User.UserHealth);
+            User.UserHealth -= User.generateAttackDamage();
+            console.log("Grant attacked " + User.name + " and decreased their health to " + User.UserHealth);
+            User.HealsRemaining --;
+            console.log(User.name + " heals Remaining is " + User.HealsRemaining);
+            Decision();
+        } else if (gameChoice === "heal" && User.HealsRemaining === 0){
+            alert("You do not have any heals remaining.");
+            Decision();
+        }   
+            
+        
+        if(gameChoice === "quit"){
+            alert("You ran from a chicken...");
+        }
+            
     }
-
-    if(UserWins===3){
-        alert("YOU KILLED THE CHICKEN!!!");
+    
+    if(User.UserHealth <= 0){
+        alert("You lost to a chicken");
     }
-
-    if(UserHealth<=0){
-        alert("you.... lost to a chicken...");
+    
+    if(User.UserWins === 5){
+        alert("you WON AND KILLED THE CHICKEN");
     }
+    
 }
