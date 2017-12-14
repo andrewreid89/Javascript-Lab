15e5ef10183651682877c0051d806bc86761cbd5
andrewreid89 <32715271+andrewreid89@users.noreply.github.com>
2017-12-13 21:40:56 -0500

    Final push for this lab

--- script.js
+++ script.js
@@ -1,13 +1,25 @@
+/*FIRST THING THE USER SEES, TO GET THEIR NAME*/
+var userName = prompt("Do you want to fight a chicken?  If you do, enter your name...", "...HERE")
+
+
+/*HIDES THE START BUTTON AND DISPLAYS THE REST OF THE GAME*/
+function startGame(){
+    document.getElementById("StartBtn").style.display = "none";
+    document.getElementById("StartGame").style.display = "block";
+}
+
+
+/*DECLARING THE OBJECTS AND ASSOCIATED PROPERTIES*/
 var character = {
-    name: "Player",
+    name: userName,
     health: 40,
     wins: 0,
-    healsRemaining: 2,
+    healsR: 2,
     generateAttackDamage: function() {
         return Math.floor(Math.random() * 3) + 1;
     },
     heal: function() {
-        this.healsRemaining--;
+        this.healsR--;
         return Math.floor(Math.random() * 10) + 1;
     }
 };
@@ -20,68 +32,71 @@ var enemy = {
     }
 };
 
-//
-//function startGame(){
-//    
-//}
-//
 
 
 
 function startCombat(choice) {
     
-    var gameText = "";
+    var gameText1 = "";
+    var gameText2 = "";
+    
 
     if (character.wins >= 5) {
-        gameText = "You have won the game!";
+        gameText1 = "CONGRATULATIONS!";
+        gameText2 = "You have won the game!"
         return;
-    }
-
-//How to get button push to link to "attack" choice; "heal" and "quit"
-    
+    }    
     
     if (choice === "attack") {
         enemy.health -= character.generateAttackDamage();
         character.health -= enemy.generateAttackDamage();
-        gameText = character.name + " has " + character.health + " health left." +
-            enemy.name + " has " + enemy.health + " health left.";
-    } else if (choice === "quit") {
-        return;
+        gameText1 = character.name + " has " + character.health + " health left."
+        gameText2 = enemy.name + " has " + enemy.health + " health left.";
+        
     } else if (choice === "heal") {
-        if (character.healsRemaining !== 0) {
+        if (character.healsR !== 0) {
             character.health += character.heal();
-            gameText = character.name + " has healed and has " + character.health + " health." +
-            character.name + " gets hit after healing and has " + character.health + " left!";
+            gameText1 = character.name + " has healed and has " + character.health + " health."
+            gameText2 = character.name + " gets hit after healing and has " + character.health + " left";
             character.health -= enemy.generateAttackDamage();
-        } else if (character.healsRemaining <= 0) {
+        } else if (character.healsR <= 0) {
             gameText = "You cannot heal!";
             character.health -= enemy.generateAttackDamage();
-            gameText = character.name + " has " + character.health + " health left." 
-            + enemy.name + " has " + enemy.health + " health left.";
+            gameText1 = character.name + " has " + character.health + " health left." 
+            gameText2 = enemy.name + " has " + enemy.health + " health left.";
         }
+        
+    }  else if (choice === "quit") {
+       document.getElementById("quitScreen").style.display = "block";
+       document.getElementById("StartGame").style.display = "none";
+        return;
     }
 
     if (character.health <= 0) {
-        gameText = "You have been defeated!";
+        gameText1 = "You have been defeated!";
+        gameText2 = "...you just lost to a chicken";
         return;
     }
     
     if (enemy.health <= 0) {
         character.wins++;
-        gameText= "You have beat " + enemy.name + "! You need to win " + (5 - character.wins) + " more round(s).";
+        gameText1 = userName + " has beat " + enemy.name + "!" 
+        gameText2 = userName + " needs to beat " + enemy.name + " " + (5 - character.wins) + " more round(s) to win the game.";
         enemy.health = 10;
     }
 
     displayInfo ();
 
-    statsInfo (gameText);
+    statsInfo (gameText1, gameText2);
 }
 
 
 
-function statsInfo (gameText){
-    var stats = document.getElementsByClassName("stats")[0];
-    stats.innerText = gameText;
+function statsInfo (gameText1, gameText2){
+    var stats1 = document.getElementsByClassName("stats1")[0];
+    stats1.innerText = gameText1;
+    var stats2 = document.getElementsByClassName("stats2")[0];
+    stats2.innerText = gameText2;
 }
 
 function displayInfo (){
@@ -92,9 +107,9 @@ function displayInfo (){
     var eName = document.getElementsByClassName("eName")[0];
     var eHealth = document.getElementsByClassName("eHealth")[0];
     name.innerText = character.name;
-    healsR.innerText = character.healsR;
-    health.innerText = character.health;
-    wins.innerText = character.wins;
+    healsR.innerText = character.healsR + " Heals Remaining";
+    health.innerText = character.health + " Health Remaining";
+    wins.innerText = character.wins + " Wins";
     eName.innerText = enemy.name;
-    eHealth.innerText = enemy.health;
+    eHealth.innerText = enemy.health + " Health Remaining";
 }
